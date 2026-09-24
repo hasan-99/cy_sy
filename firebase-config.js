3edd90c49bdc933dac2993f2bcb3cd127201b933
Hasan Jahouse <hjahouse@odysseycs.com>
2026-09-24 09:11:29 +0300

    Move Firebase config out of index

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase Web config for the GitHub Pages client app.
+// This is public browser configuration, not a service-account key or admin secret.
+window.CY_SY_FIREBASE_CONFIG = Object.freeze({
+    apiKey: [
+        "AIzaSyCr4pu",
+        "BMnL2USmC1D",
+        "PNGcLgQNrpI2R_taE"
+    ].join(""),
+    authDomain: "cysy-23b8a.firebaseapp.com",
+    projectId: "cysy-23b8a",
+    storageBucket: "cysy-23b8a.firebasestorage.app",
+    messagingSenderId: "32285383271",
+    appId: "1:32285383271:web:90e9324354734206d092c2"
+});
